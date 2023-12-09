922366a69b2cb39bc3c1330870b4cec9781a7c36
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2023-12-09 13:35:09 +0600

    second save

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,8 +3,10 @@ const cookie = document.getElementById("cookie");
 function changeSizes() {
   if (clicNumber.textContent % 2 === 0) {
     cookie.width = 300;
+    cookie.height = 300;
   } else {
     cookie.width = 200;
+    cookie.height = 200;
   }
   clicNumber.textContent++;
 }
