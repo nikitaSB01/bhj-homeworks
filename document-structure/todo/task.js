a5588b54e80db7b59327a34f817ba947ef8844b0
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2023-12-26 23:28:02 +0600

    change task 3

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,27 +4,21 @@ const ListTask = document.querySelector(".tasks__list");
 
 taskButton.addEventListener("click", (elem) => {
   elem.preventDefault();
-  if (taskInput.value === "") {
-    return;
-  } else {
-    ListTask.insertAdjacentHTML(
-      "afterBegin",
-      "<div class='task'><div class='task__title'></div><a href='#' class='task__remove'>&times;</a></div>"
-    );
-    let title = document.querySelectorAll(".task__title");
-    for (let i = 0; i < title.length; i++) {
-      if (!title[i].textContent) {
-        title[i].textContent = taskInput.value;
-      }
-    }
+  let valueTaskInput = taskInput.value;
+  if (taskInput.value.trim() === "") {
     taskInput.value = null;
+    return;
   }
-  let task = document.getElementsByClassName("task");
-  let taskRemove = document.getElementsByClassName("task__remove");
+  ListTask.insertAdjacentHTML(
+    "afterBegin",
+    `<div class='task'><div class='task__title'>${valueTaskInput}</div><a href='#' class='task__remove'>&times;</a></div>`
+  );
+  let title = document.querySelectorAll(".task__title");
+  taskInput.value = null;
 
-  for (let i = 0; i < taskRemove.length; i++) {
-    taskRemove[i].addEventListener("click", function () {
-      this.parentElement.remove();
-    });
-  }
+  let task = document.querySelector(".task");
+  let taskRemove = document.querySelector(".task__remove");
+  taskRemove.addEventListener("click", function () {
+    this.parentElement.remove();
+  });
 });
