e7d8e8f39565a6f67a1ec3190a0170e5b143990c
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2023-12-26 22:20:06 +0600

    change task 2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,28 +1,23 @@
+const header = document.querySelector(".header");
 const hasTooltips = Array.from(document.querySelectorAll("a.has-tooltip"));
+header.insertAdjacentHTML(
+  "beforeBegin",
+  "<div class='tooltip' style=''></div>"
+);
 let divTooltip = document.querySelector(".tooltip");
-
 for (const item of hasTooltips) {
   item.addEventListener("click", (elem) => {
     elem.preventDefault();
-    let text = item.title;
-    if (item.children[0]) {
-      item.children[0].remove();
+    let titleTextItem = item.title;
+    if (divTooltip.textContent === titleTextItem) {
+      divTooltip.classList.toggle("tooltip_active");
     } else {
-      let divElements = Array.from(
-        document.querySelectorAll(".tooltip_active")
-      );
-      if (divElements.length >= 1) {
-        console.log("ERROR");
-      } else {
-        let divElem = document.createElement("div");
-        divElem.className = "tooltip";
-        divElem.textContent = text;
-        item.appendChild(divElem);
-        item.style.position = "relative";
-        divElem.style.position = "absolute";
-        divElem.style.left = "0";
-        divElem.classList.toggle("tooltip_active");
-      }
+      divTooltip.classList.add("tooltip_active");
+      divTooltip.textContent = titleTextItem;
+      item.append(divTooltip);
+      item.style.position = "relative";
+      divTooltip.style.position = "absolute";
+      divTooltip.style.left = "0";
     }
   });
 }
