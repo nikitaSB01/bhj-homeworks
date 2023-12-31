60afab39e3c7bd26e1fcc840bcb18162859fb5aa
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2023-12-31 12:43:33 +0600

    Task 2 without complications

---
+++ async-requests/poll/task.js
@@ -0,0 +1,23 @@
+let pollTitle = document.querySelector(".poll__title");
+const pollAnswers = document.querySelector(".poll__answers");
+//
+const xhr = new XMLHttpRequest();
+xhr.addEventListener("readystatechange", () => {
+  if (xhr.readyState === xhr.DONE) {
+    const result = JSON.parse(xhr.response);
+    pollTitle.textContent = result.data.title;
+    for (let i = 0; i < result.data.answers.length; i++) {
+      pollAnswers.insertAdjacentHTML(
+        "beforeEnd",
+        `<button class='poll__answer'>${result.data.answers[i]}</button>`
+      );
+      const buttons = Array.from(document.querySelectorAll(".poll__answer"));
+      buttons[i].addEventListener("click", () => {
+        alert("Спасибо, ваш голос засчитан!");
+      });
+    }
+  }
+});
+
+xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/poll");
+xhr.send();
